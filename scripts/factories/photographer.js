a1a8acd558093e3d79ca46a1654f1872603ebcda
PalaciosCaroline <caroline2807@yahoo.fr>
2022-08-05 23:29:50 +0200

    création de la page index en js, enregistrement des images

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,62 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+    const { id, name, portrait, city, country, tagline, price } = data;
+    const picture = `assets/photographers/photo_choisi/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        const a = document.createElement('a');  
+        a.href = `./photographer.html?id=${id}`;
+        const imgChoisi = document.createElement( 'img' );
+        imgChoisi.setAttribute("src", picture);
+        imgChoisi.alt = name;
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = `${city}, ${country}`;
+        const legende = document.createElement( 'p' );
+        legende.textContent = tagline;
+        const prices = document.createElement( 'span' );
+        prices.textContent = `${price} euros/jours`;
+        article.appendChild(a)
+        // a.appendChild(divimg);
+        // divimg.appendChild(img);
+        a.appendChild(imgChoisi);
+        a.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(legende);
+        article.appendChild(prices);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
+
+
+// function PageHeaderFactory(_photographerChoice) {
+//     const { name, portrait, city, country, tagline } = data;
+//     const picture = `assets/photographers/photographers_ID_Photos/${portrait}`;
+
+
+//   function getUserHeaderDOM() {
+//         const header = document.getElementById( 'photograph-header' );
+//         const imgChoisi = document.createElement( 'img' );
+//         imgChoisi.setAttribute("src", picture);
+//         imgChoisi.alt = name;
+//         const h2 = document.createElement( 'h2' );
+//         h2.textContent = name;
+//         const h3 = document.createElement( 'h3' );
+//         h3.textContent = `${city}, ${country}`;
+//         const legende = document.createElement( 'p' );
+//         legende.textContent = tagline;
+//         article.appendChild(imgChoisi);
+//         article.appendChild(h2);
+//         article.appendChild(h3);
+//         article.appendChild(legende);
+//         return (article);
+//     }
+//     return { name, picture, city, country, tagline, price, getUserHeaderDOM }
+// }
+
+
+// export {photographerFactory};
+// export {PageHeaderFactory};
+   
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,57 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Mettre le code JavaScript lié à la page photographer
+// import { getPhotographers } from "./index.js";
+// import { getMedias } from "./index.js";
+// import {PageHeaderFactory} from "../factories/photographer.js";
+
+let params = new URLSearchParams(document.location.search);
+let photographerId = params.get("id"); // 
+
+// photographerChoice est undefined (n'arrive pas à utiliser photographers)
+// const photographerChoice = photographers.find((photographer) => photographer.id === photographerId);
+
+
+async function getPhotographers() {
+
+    const response = await fetch("./data/photographers.json");
+       const data = await response.json();
+      const photographers = data.photographers;
+        // .catch((error) =>
+        //      console.log('Il y a eu un problème avec l\'opération fetch'));
+
+        // const photographerChoice = photographers.find((photographer) => photographer.id === photographerId);
+        console.log(photographers);
+   return photographers;
+}
+
+ async function getMedias() {
+    const response = await fetch("./data/photographers.json");
+    const data = await response.json();
+   const media = data.media;
+   let photographerChoiceMedia = media.filter((media) => media.photographerId === photographerId);
+     // .catch((error) =>
+     //      console.log('Il y a eu un problème avec l\'opération fetch'));
+    return photographerChoiceMedia;
+}
+
+
+// async function displayData(photographerChoice) {
+//     const photographersHeader = document.querySelector(".photograph-header");
+//         const photographerModel = PageHeaderFactory(photographerChoice);
+//         const userHeaderDOM = photographerModel.getUserCardDOM();
+//         photographersHeader.appendChild(userHeaderDOM);
+// };
+
+
+
+async function initPage() {
+    const photographers = await getPhotographers();
+    const media = await getMedias();
+    // const photographerChoice = photographers.find((photographer) => photographer.id === photographerId);
+  
+  
+    // displayData(photographerChoice);
+   
+}
+
+initPage();
+
