a7672359cf59a9436b2b2f0c1f4bcd1e8156e908
PalaciosCaroline <caroline2807@yahoo.fr>
2022-08-08 14:10:41 +0200

    Dry sur la factorypattern

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -50,117 +50,83 @@ const counterLikes = document.getElementById('counterLikes');
 
 async function displayData(photographer,media) {
   
-
         //header photograph
-      const contactbtn = document.querySelector('button');
-      const photographerModel = PageHeaderFactory(photographer);
-      const getUserHeaderDOM = photographerModel.getUserHeaderDOM();
-      const imgPortrait = document.querySelector(".photograph-header > img")
-      const header = document.querySelector(".photograph-header");
-      imgPortrait.parentNode.insertBefore(contactbtn,imgPortrait);
+        // const photographerModel = PageMediaFactory(photographer,media);
+        const mediaModel = PageMediaFactory(photographer,media);
+        const getUserHeaderDOM = mediaModel.getUserHeaderDOM();
 
       const boxmedia = document.getElementById('boxmedia');
 
       const TotalLikes = media.map(item => item.likes).reduce((prev, curr) => prev + curr, 0);
 
-    
-
-     
-
       //box card media
       media.forEach((item) => {
-        const mediaModel = PageMediaFactory(item,TotalLikes);
-        const getMediaCardDOM = mediaModel.getMediaCardDOM()
+        const mediaModelCard = PageMediaFactory(photographer,item);
+        const getMediaCardDOM = mediaModelCard.getMediaCardDOM()
         boxmedia.appendChild(getMediaCardDOM);
       })
 
-      const mediaLikes = PageMediaFactory(media,TotalLikes);
-      const TotalLikeArticle = mediaLikes.getTotalLikes(photographer.price);
+      //étiquette du bas de page avec le Total des likes et du tarif/jour du photograph
+      const TotalLikeArticle = mediaModel.getTotalLikes();
       boxmedia.appendChild(TotalLikeArticle);
-  
 
       const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
-    
+
+      //Nécessaire pour le Compteur console.log du total likes
       totalLikesTitle.textContent = TotalLikes;
+      
       // A revoire
       // btnLikes.addEventListener('click', () => {                 
       //   btnLiketitle.innerHTML = likes + 1;
       //   const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
       //   totalLikesTitle.textContent = parseInt(totalLikesTitle.textContent) + 1 ;
       // })
-      
 
-      // const likestitle = document.getElementsByClassName('likestitle');
-      // console.log(likestitle)
-   
-      
-      //Sum Likes Total
-      // const newlikes = media.likes;
-     
-      // const btnLikes = document.getElementsByClassName( 'likestitle' );
-      // console.log(btnLikes)
-      // btnLikes.textContent = newlikes;
-
-      // btnLikes.forEach((item) => {item.addEventListener('click', () => {
-      //   const btnLikes = document.getElementsByClassName( 'likestitle' );
-    
-
-        //A finir modal contact 
-        const h2 = document.querySelector("#contact_modal > h2");
-        const modalModel = getModal(photographer);
-        const getUserModalDOM = modalModel.getUserModalDOM();
-        const name = document.querySelector("header > .name");
-
-  }
+      //A finir modal contact 
+      const h2 = document.querySelector("#contact_modal > h2");
+      const getUserModalDOM = mediaModel.getUserModalDOM();
+      // const name = document.querySelector("header > .name");
+}
 
 
 async function initPage() {
     const photographer = await getPhotograph();
     const media = await getMedias();
     displayData(photographer,media);
-
-   
-    
 }
 
 initPage();
 
 
-function PageHeaderFactory(photograph) {
-  const { id, name, portrait, city, country, tagline, price } = photograph;
-  const picture = `assets/photographers/photographers_ID_Photos/${portrait}`;
+function PageMediaFactory(photograph,data) {
+  const imgHeart = `../assets/icons/heart`;
+  const pictureVideo = `../assets/${data.photographerId}/${data.video}`;
+  const pictureImg = `../assets/${data.photographerId}/${data.image}`;
+  const picture = `assets/photographers/photographers_ID_Photos/${photograph.portrait}`;
 
   function getUserHeaderDOM() {
       const header = document.querySelector(".photograph-header");
+      const contactbtn = document.querySelector('button');
       const article = document.createElement( 'article' );
       const h2 = document.createElement( 'h2' );
-      h2.textContent = name;
+      h2.textContent = photograph.name;
       const h3 = document.createElement( 'h3' );
-      h3.textContent = `${city}, ${country}`;
+      h3.textContent = `${photograph.city}, ${photograph.country}`;
       const legende = document.createElement( 'p' );
-      legende.textContent = tagline;
+      legende.textContent = photograph.tagline;
       const imgChoisi = document.createElement( 'img' );
       imgChoisi.setAttribute("src", picture);
-      imgChoisi.alt = name;
+      imgChoisi.alt = photograph.name;
       header.appendChild(article);
       article.appendChild(imgChoisi);
       article.appendChild(h2);
       article.appendChild(h3);
       article.appendChild(legende);
       header.appendChild(imgChoisi);
-      
+      imgChoisi.parentNode.insertBefore(contactbtn,imgChoisi);
+
       return (article);
-  }
-  return { name, city, country, tagline, getUserHeaderDOM }
-}
-
-
-
-function PageMediaFactory(data,total) {
-  const {  id, photographerId, title, image, video, likes, price,date} = data;
-  const imgHeart = `../assets/icons/heart`;
-  const pictureVideo = `../assets/${photographerId}/${video}`;
-  const pictureImg = `../assets/${photographerId}/${image}`;
+    }
 
   function getMediaCardDOM() {
      
@@ -172,32 +138,32 @@ function PageMediaFactory(data,total) {
       const divmedia = document.createElement('div'); 
       divmedia.classList.add('card_media');
       a.appendChild(divmedia);
-        if (image) {
+        if (data.image) {
           const imgphoto = document.createElement( 'img' );
           imgphoto.classList.add('cardImg');
           imgphoto.setAttribute("src", pictureImg);
-          imgphoto.alt = title;
+          imgphoto.alt = data.title;
           divmedia.appendChild(imgphoto);
           // return imgphoto;
           
-      } else if (video) {
+      } else if (data.video) {
           const videophoto = document.createElement( 'video' );
           videophoto.setAttribute("src", pictureVideo);
           videophoto.setAttribute("controls", true);
           videophoto.controls = true;
-          videophoto.alt = title;
+          videophoto.alt = data.title;
           divmedia.appendChild(videophoto);
         // return videophoto;
       }
       const divtext = document.createElement('div'); 
       divtext.classList.add('card_text');
       const titlemedia = document.createElement( 'h2' );
-      titlemedia.textContent = title;
+      titlemedia.textContent = data.title;
       const btnLikes = document.createElement( 'button' );
       btnLikes.classList.add('btnLikes');
       const btnLiketitle = document.createElement( 'span' );
       btnLiketitle.classList.add('likestitle');
-      btnLiketitle.textContent = likes;
+      btnLiketitle.textContent = data.likes;
 
       const heart = document.createElement( 'img' );
       heart.setAttribute("src", `../assets/icons/heart.png`);
@@ -209,44 +175,35 @@ function PageMediaFactory(data,total) {
       divtext.appendChild(btnLikes);
       btnLikes.appendChild(btnLiketitle);
       btnLikes.appendChild(heart);
+      
 
-      //increment partielLikes
-      
-      // btnLikes.addEventListener('click', () => {                 
-      //             btnLiketitle.innerHTML = likes + 1;
-      //             const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
-      //             totalLikesTitle.textContent = parseInt(totalLikesTitle.textContent) + 1 ;
-      //           })
-      
+      //A refaire increment partielLikes
       const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
-    
-      totalLikesTitle.textContent = total;
       btnLikes.addEventListener('click', () => { 
         console.log(totalLikesTitle);
         btnLiketitle.textContent = parseInt( btnLiketitle.textContent) + 1;
-        const toto = totalLikesTitle.textContent ;
-          totalLikesTitle.textContent = parseInt( totalLikesTitle.textContent) + 1;
-
-    })
-
-
+        totalLikesTitle.textContent = parseInt( totalLikesTitle.textContent) + 1;
+      })
 
       return (article);
     }
 
-    function getTotalLikes(price) {
+    
+    function getTotalLikes() {
+
+      const totallikes = data.map(item => item.likes).reduce((prev, curr) => prev + curr, 0);
 
       const TotalLikessum = document.createElement( 'article' );
       const divlike = document.createElement( 'div' );
       TotalLikessum.classList.add('cardLikes');
       const totalLiketitle = document.createElement( 'span' );
       totalLiketitle.classList.add('totalLikesTitle');
-      totalLiketitle.textContent = total;
+      totalLiketitle.textContent = totallikes;
       const heart2 = document.createElement( 'img' );
       heart2.setAttribute("src", `../assets/icons/heartblack.png`);
       heart2.alt = "likes";
       const pricejour = document.createElement( 'span' );
-      pricejour.textContent = `${price}€ / jour`;
+      pricejour.textContent = `${photograph.price}€ / jour`;
 
       TotalLikessum.appendChild(divlike);
       divlike.appendChild(totalLiketitle);
@@ -256,49 +213,45 @@ function PageMediaFactory(data,total) {
       return (TotalLikessum);
     }
 
-
-    function getsorting() {
-      
+    function getUserModalDOM() {
+      const header = document.querySelector("#contact_modal > .modal > header");
+      // const titre = document.querySelector("#contact_modal > header > h2");
+      const h2 = document.createElement( 'h2' );
+      h2.classList.add("name");
+      h2.textContent = photograph.name;
+      // header.appendChild(div);
+      header.appendChild(h2);
+      return (h2);
     }
 
-  return { date, id, image,video, likes, photographerId, price, title , getMediaCardDOM, getTotalLikes 
-  }
+  return {getUserHeaderDOM, getMediaCardDOM, getTotalLikes, getUserModalDOM }
 }
 
 
 
-function getModal(photograph) {
-  const { id, name, portrait, city, country, tagline, price } = photograph;
+//   function getModal(photograph) {
+ 
 
-  function getUserModalDOM() {
-        const header = document.querySelector("#contact_modal > .modal > header");
-        // const titre = document.querySelector("#contact_modal > header > h2");
-        const h2 = document.createElement( 'h2' );
-        h2.classList.add("name");
-        h2.textContent = name;
-        // header.appendChild(div);
-        header.appendChild(h2);
-        return (h2);
-    }
-  return { name, city, country, tagline, getUserModalDOM }
-}
+//   function getUserModalDOM() {
+//         const header = document.querySelector("#contact_modal > .modal > header");
+//         // const titre = document.querySelector("#contact_modal > header > h2");
+//         const h2 = document.createElement( 'h2' );
+//         h2.classList.add("name");
+//         h2.textContent = photograph.name;
+//         // header.appendChild(div);
+//         header.appendChild(h2);
+//         return (h2);
+//     }
+//   return { getUserModalDOM }
+// }
 
-// function ecoute(){
-// const btnLikes = document.getElementsByClassName('btnLikes');
-// btnLikes.forEach((item) => item.addEventListener('click', () => {
-//           const likestitle = document.getElementsByClassName('likestitle');
-//           likestitle.innerHTML = likes + 1;
-//           const TotalLiketitle = document.getElementsByClassName('totalLikesTitle');
-//            TotalLiketitle.innerHTML = total +1;
-// }))}
-
-function incrementeLikes(){
-const btnLikes = document.querySelectorAll("btnLikes");
-const likestitle = document.querySelectorAll("likestitle");
-      console.log(btnLikes);
-      const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
-      for (let i = 0; i < btnLikes.length; i++) {
-        btnLikes[i].addEventListener("click", () => {
-          likestitle[i].textContent = parseInt(likestitle[i].textContent) + 1;
-          totalLikesTitle.textContent = parseInt( totalLikesTitle.textContent) + 1;
-})}};
+// function incrementeLikes(){
+// const btnLikes = document.querySelectorAll("btnLikes");
+// const likestitle = document.querySelectorAll("likestitle");
+//       console.log(btnLikes);
+//       const totalLikesTitle = document.getElementsByClassName('totalLikesTitle');
+//       for (let i = 0; i < btnLikes.length; i++) {
+//         btnLikes[i].addEventListener("click", () => {
+//           likestitle[i].textContent = parseInt(likestitle[i].textContent) + 1;
+//           totalLikesTitle.textContent = parseInt( totalLikesTitle.textContent) + 1;
+// })}};
