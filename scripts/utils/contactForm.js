d3ce0b72b957a563c6baa3321fe54da230cd0ad4
PalaciosCaroline <caroline2807@yahoo.fr>
2022-08-13 12:26:47 +0200

    modification du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,30 +1,66 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+//dom elements
+const modal = document.getElementById("contact_modal");
+const formContact = document.getElementById('form_contact');
+
+const entries = document.querySelectorAll('.entry');
+const header = document.querySelector("#contact_modal > .modal > header");
+const h2 = modal.querySelectorAll('h2');
+const messageSend = document.getElementById("message_send");
+const btnclose = header.querySelector('img');
+const main = document.getElementById("main");
+
+export function getUserModalDOM(photographer) {
+    const h2 = document.createElement( 'h2' );
+    h2.classList.add("name");
+    h2.textContent = photographer.name;
+    header.appendChild(h2);
+    messageSend.innerHTML = `<div id="message_send" class="block">Merci<br>Votre message a bien été envoyé à <br>${photographer.name}.         
+    </div>`
+   
+    formContact.addEventListener("submit", (event) => {
+    event.preventDefault();
+    logUserInformations();
+    }) 
+
+    btnclose.addEventListener("click", closeModal);
+
+  return (formContact);
+}
+
+export function displayModal() {
+    h2.forEach(title => title.classList.remove("hidden"));
+    modal.style.display = "block";
+    formContact.style.display = "block";
+    formContact.focus();
+    messageSend.classList.add('hidden');
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    messageSend.classList.add('hidden');
+    main.focus();
     modal.style.display = "none";
 }
 
 function logUserInformations() {
-    const formContact = document.getElementById('form_contact');
-    const firstName = document.getElementById("prenom");
-    const lastName = document.getElementById("nom");
-    const email = document.getElementById("email");
-    const message = document.getElementById("message");
+        console.log(formContact)
     const userInformation = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        message: message.value
+        firstName: formContact[0].value,
+        lastName: formContact[1].value,
+        email: formContact[2].value,
+        message: formContact[3].value
     };
     console.log("Données fournies par l'utilisateur: ")
     for (let property in userInformation) {
         console.log(`${property}: ${userInformation[property]}`)
     }
-    for (let property in formContact) {
-        formContact[property].value = '';
+    
+    for (let i=0 ;i < entries.length ; i++) {
+        formContact[i].value = '' ;
     }
+   
+    formContact.style.display = "none";
+    const h2 = modal.querySelectorAll('h2')
+    h2.forEach(title => title.classList.add("hidden"));
+    messageSend.classList.remove('hidden');
 }
+
